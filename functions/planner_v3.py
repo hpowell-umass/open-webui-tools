a1ac931c234cdb151d90fa1679869835f1a2fa7a
Haervwe <juan-jose-007@hotmail.com>
2026-04-04 17:06:59 -0300

    chore: bump planner_v3 version to 3.9.0

--- functions/planner_v3.py
+++ functions/planner_v3.py
@@ -3,7 +3,7 @@ title: Planner v3
 author: Haervwe
 author_url: https://github.com/Haervwe
 funding_url: https://github.com/Haervwe/open-webui-tools
-version: 3.8.1
+version: 3.9.0
 required_open_webui_version: 0.8.12
 
 Features:
